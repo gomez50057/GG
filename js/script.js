328c7951a3e0f6dbcbc67f7ef39ab1375ee987a5
gomez50057 <37481091+gomez50057@users.noreply.github.com>
2022-08-02 16:51:15 -0500

    boton Up - titulo- coloN-skill-banner

--- js/script.js
+++ js/script.js
@@ -22,16 +22,22 @@ function responsiveMenu() {
 //detecto el scrolling para aplicar la animación del la barra de habilidades
 window.onscroll = function() { efectoHabilidades() };
 
-//funcion que aplica la animación de la barra de habilidades
-function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 300) {
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
+//Boton que sube 
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
     }
-
-}
-
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  
