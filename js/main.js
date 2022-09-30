3082fdf498e74bdb6c2c3fc463724e4c8dc559ce
gomez50057 <37481091+gomez50057@users.noreply.github.com>
2022-09-30 10:29:59 -0500

    dark portafolio

--- js/main.js
+++ js/main.js
@@ -24,4 +24,31 @@
 
 })();
 
+//moon y sun
+const btnSwitch = document.querySelector("#switch")
+btnSwitch.addEventListener('click',() =>{
+  document.body.classList.toggle("dark");
+  btnSwitch.classList.toggle("active");
+});
+
+
+//Boton que sube 
+window.onscroll = function(){
+    //console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 10) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
+    }
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
 
